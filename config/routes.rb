fccd22cb6a2aa3ce23ac7399aa490ae97f4eabf4
yuyaya777 <yuyaya777@gmail.com>
2021-03-20 05:05:33 +0000

    Commit7

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to: 'homes#top'
+  get 'top' => 'homes#top', as: 'top'
+  get 'about' => 'homes#about', as: 'about'
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:edit, :index, :show]
 
