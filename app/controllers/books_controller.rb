7aaab4507df480fea90565b9ab2dbd9a8092dadd
yuyaya777 <yuyaya777@gmail.com>
2021-03-21 13:25:28 +0000

    Commit13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,17 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(book_params)
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'top' => 'homes#top', as: 'top'
   get 'about' => 'homes#about', as: 'about'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:edit, :index, :show, :update]
 end
