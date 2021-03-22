d38e15cb30ca118095143960fe6694ac8e5dd23a
yuyaya777 <yuyaya777@gmail.com>
2021-03-22 12:45:34 +0000

    Commit17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @user = current_user
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page])
@@ -18,6 +25,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
